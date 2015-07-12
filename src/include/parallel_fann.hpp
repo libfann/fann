f9e5c39ec61120b518911335cfdf480c9546c0a1
Thomas Laroche <tho.laroche@gmail.com>
2015-07-12 17:33:09 +0200

    Fix parallel headers:
    - Add #ifndef FIXEDFANN
    - Add extern "C" to parallel_fann.h

--- src/include/parallel_fann.hpp
+++ src/include/parallel_fann.hpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <fann.h>
 
+#ifndef FIXEDFANN
 namespace parallel_fann {
 float train_epoch_batch_parallel(struct fann *ann, struct fann_train_data *data, const unsigned int threadnumb);
 
@@ -33,5 +34,6 @@ float train_epoch_incremental_mod(struct fann *ann, struct fann_train_data *data
 float test_data_parallel(struct fann *ann, struct fann_train_data *data, const unsigned int threadnumb);
 float test_data_parallel(struct fann *ann, struct fann_train_data *data, const unsigned int threadnumb, std::vector< std::vector<fann_type> >& predicted_outputs);
 }
+#endif /* FIXEDFANN */
 #endif /* PARALLEL_FANN_HPP_ */
 #endif /* DISABLE_PARALLEL_FANN */
