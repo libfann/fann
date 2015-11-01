e508d01497641625ce2d196f73c22e00b18a07e6
steffennissen <steffen.fann@gmail.com>
2015-11-01 17:41:34 +0800

    refactoring of the C++ headers and associated documentation

--- docs/generate_docs.sh
+++ docs/generate_docs.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=`dirname $0`
 
-${SCRIPT_PATH}/NaturalDocs-1.52/NaturalDocs -i ${SCRIPT_PATH}/../src/include/ -o HTML ${SCRIPT_PATH}/html -p ${SCRIPT_PATH}/prj
+${SCRIPT_PATH}/NaturalDocs-1.52/NaturalDocs -r -i ${SCRIPT_PATH}/../src/include/ -o HTML ${SCRIPT_PATH}/html -p ${SCRIPT_PATH}/prj
