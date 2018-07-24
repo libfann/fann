66ee89a352d85816cb5e000c4004695d8fc889ad
Nico Madysa <nico.madysa@tu-dresden.de>
2018-07-24 22:25:40 +0200

    Check in ScaleDataByANNError that data is left unmodified.

--- tests/fann_test_data.cpp
+++ tests/fann_test_data.cpp
@@ -199,12 +199,16 @@ TEST_F(FannTestData, ScaleDataByANNError) {
 
     net.scale_input(input);
     EXPECT_EQ(net.get_errno(), fann_errno_enum::FANN_E_SCALE_NOT_PRESENT);
+    EXPECT_EQ(input[0], 0.0);
+    EXPECT_EQ(input[1], 1.0);
+    EXPECT_EQ(input[2], 0.5);
 
     net.reset_errno();
     EXPECT_EQ(net.get_errno(), fann_errno_enum::FANN_E_NO_ERROR);
 
     net.scale_output(output);
     EXPECT_EQ(net.get_errno(), fann_errno_enum::FANN_E_SCALE_NOT_PRESENT);
+    EXPECT_EQ(output[0], 1.5);
 
     net.reset_errno();
     EXPECT_EQ(net.get_errno(), fann_errno_enum::FANN_E_NO_ERROR);
@@ -212,4 +216,8 @@ TEST_F(FannTestData, ScaleDataByANNError) {
     data.set_train_data(1, 3, input, 1, output);
     net.scale_train(data);
     EXPECT_EQ(net.get_errno(), fann_errno_enum::FANN_E_SCALE_NOT_PRESENT);
+    EXPECT_EQ(data.get_input()[0][0], 0.0);
+    EXPECT_EQ(data.get_input()[0][1], 1.0);
+    EXPECT_EQ(data.get_input()[0][2], 0.5);
+    EXPECT_EQ(data.get_output()[0][0], 1.5);
 }
